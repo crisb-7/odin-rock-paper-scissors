294eb99ef31df07bc236d58dfb8f7b735f57d1b4
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-05 08:23:07 -0600

    New version of Rock Paper Scissors, with an extremely rudimentary UI

--- script.js
+++ script.js
@@ -1,82 +1,80 @@
-function game() {
-    alert("Rock, Paper, Scissors! First to 5 against the computer!\n");
-    let playerWins = 0;
-    let computerWins = 0;
-    while (true) {
-        if (playerWins >= 5) {
-            alert("You won the game! \nThanks for playing!");
-            break;
-        } else if (computerWins >= 5) {
-            alert("You lost the game! \nThanks for playing!");
-            break;
-        } else {
-            play = prompt("Enter your choice, or press Enter to exit.\n");
-            let result;
-            if (play == "" || play === null) {
-                alert("Thanks for playing!");
-                break;
-            } else {
-                result = round(play);
-            }
-            let txtResult;
-            switch (result) {
-                case 1:
-                    playerWins += 1;
-                    txtResult = "You won this round!";
-                    console.log(txtResult);
-                    break;
-                case 0:
-                    computerWins += 1;
-                    txtResult = "You lost this round!";
-                    console.log(txtResult);
-                    break;
-                case -1:
-                    txtResult = "It's a tie!";
-                    console.log(txtResult);
-                    break;
-                default:
-                    console.log("Sorry, something unexpected happened");
-            }
-            let score = `Score:\n Player - ${playerWins}\n Computer - ${computerWins}`;
-            console.log(score);
-        }
-    }
-}
-
 function round(choice) {
-    choice = standardizeInput(choice);
-    choice = choiceToInt(choice);
     let computerChoie = getComputerChoice();
+    // 2-d array to determine the winner, array indices are player and computer choices
     let resultMatrix = [[-1, 0, 1], [1, -1, 0], [0, 1, -1]]; // Win - 1, Lose - 0, Tie - (-1)
     return resultMatrix[choice][computerChoie];
 }
 
 function getComputerChoice() {
-    number = randomInt(1, 2);     // Rock - 0, Paper - 1, Scissors - 2
+    number = randomInt(0, 2);     // Rock - 0, Paper - 1, Scissors - 2
     return number;
-}
-
-function standardizeInput(choice) {
-    choice = choice.toLowerCase();
-    choice = choice.replace(choice.substring(0, 1), choice.substring(0, 1).toUpperCase());
-    return choice;
-}
-
-function choiceToInt(choice) {
-    switch (choice) {
-        case "Rock":
-            return 0;
-        case "Paper":
-            return 1;
-        case "Scissors":
-            return 2;
-        default:
-            console.log("Sorry, something unexpected happened");
-    }
 }
 
 function randomInt(min, max) {
     return Math.round(Math.random() * (max - min)) + min;
 }
 
-game()
+function determineRoundWinner(result) {
+    switch (result) {
+        case 1:
+            playerScore += 1;
+            writeRound("You won this round!");
+            displayScore();
+            break;
+        case 0:
+            botScore += 1;
+            writeRound("You lost this round!");
+            displayScore();
+            break;
+        case -1:
+            writeRound("It's a tie!");
+            displayScore();
+            break;
+        default:
+            writeRound("Sorry, something unexpected happened");
+    }
+}
+
+function determineMatchWinner() {
+    if (playerScore >= maxWins) {
+        writeRound("You won the match!");
+    } else if (botScore >= maxWins) {
+        writeRound("You lost the match!");
+    } else {
+        return;
+    }
+    playerScore = 0;
+    botScore = 0;
+    writeRound("Thanks for playing");
+}
+
+function writeRound(text) {
+    let result = document.createElement("div");
+    const body = document.querySelector("body")
+    result.innerHTML = "<div>" + text + "</div>";
+    body.append(result);
+}
+
+function displayScore() {
+    let score;
+    score = `<ul><li>Player - ${playerScore}</li> <li>Computer - ${botScore}</li>`;
+    writeRound(score);
+}
+
+function playRound(selection) {
+    let roundResult = round(selection);
+    determineRoundWinner(roundResult);
+    determineMatchWinner();
+}
+
+
+let playerScore = 0;
+let botScore = 0;
+let maxWins = 5;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let playerSelection = parseInt(button.id);
+        playRound(playerSelection);
+    });
+});
